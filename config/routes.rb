e8c49d48cf5ce9f50cc39464b3c3d50d9c25f425
Iwanaga-Sena <iwanaga.sena.infratop@gmail.com>
2024-08-19 07:01:11 +0000

    [add] view controller model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get "home/about" => "homes#about", as: 'about'
+  
+  devise_for :users
+  
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+  end
+  
+  resources :book_comments, only: [:create, :destroy]
+  
+  resources :users, only: [:index, :show, :edit, :update]
 end
